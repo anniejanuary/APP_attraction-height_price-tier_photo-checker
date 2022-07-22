f8e808e93cce7093165c93a000e999fe4833052d
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-22 19:04:52 +0200

    Update APP_ride-height-and-price-checker.py

--- APP_ride-height-and-price-checker.py
+++ APP_ride-height-and-price-checker.py
@@ -1,13 +1,26 @@
 print("Welcome to our rollercoaster ride!")
 height = int (input ("What is your height in cm?: "))
 
+bill = 0
+
 if height >= 120:
   age = int (input ("How old are you?: "))
   if age <= 12:
-    print("It will be $5")
+    bill = 5
+    print(f"Child tickets are ${bill}")
   elif age >12 and age <= 18:
-    print("It will be $7")
+    bill = 7
+    print(f"Teenager tickets are ${bill}")
   else:
-    print("It will be $12")
+    bill = 12
+    print(f"Adult tickets are ${bill}")
+    
+  photo = input ("Do you want a $3 photo? Y / N")
+  if photo == "Y":
+    bill += 3
+    print(f"Your total is ${bill}")
+  else:
+    print(f"It will be ${bill}") 
+                
 else:
   print("You still need to grow taller to ride")
